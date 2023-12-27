11422407233ca9a7fbcee71d592046d07d85ce3d
Saidul Islam <saidulislam197120@gmail.com>
2023-12-27 15:04:07 +0600

    Add some text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
                 </div>
 
                 <h1 class="text-center mt-16">DEMO RESTURANT FOR OPTIMIZE SERVER PERFORMANCE</h1>
-
+                <p class="text-center mt-8">That helps to make best ....</p>
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
